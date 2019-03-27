4150d1bd019d15464abe0cf815b8cab89fdb99c1
Anna Konstantinova <konstantinova.anouk@gmail.com>
2019-03-27 22:23:05 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+const costs = [200, 550, 4000, 23, 58, 5000, 485, 711];
+
+const getBonus = (arr) => {
+    let bonus = 0;
+    let sum = 0;
+    for (let value of arr) {
+        sum += value;
+    };
+    if (sum > 10000) {
+        bonus = 0.05*sum;
+    } 
+    return parseInt(bonus);
+};
+
+console.log(getBonus(costs));
+
